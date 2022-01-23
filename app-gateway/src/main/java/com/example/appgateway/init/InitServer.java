efbe3db8e037cb544d990dcd1b0b3e9ee670aa7e
wangwc <wwc953@163.com>
2022-01-23 13:47:32 +0800

    getway

--- app-gateway/src/main/java/com/example/appgateway/init/InitServer.java
+++ app-gateway/src/main/java/com/example/appgateway/init/InitServer.java
@@ -14,6 +14,6 @@ public class InitServer implements ApplicationListener<ApplicationStartedEvent> 
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
-        gatwayDataService.loadRouteData();
+//        gatwayDataService.loadRouteData();
     }
 }
