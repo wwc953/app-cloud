60295dfb9416f1a4dc4b62bab905c5b1e831a0fb
wangwc <wwc953@163.com>
2022-02-11 19:42:15 +0800

    idddd

---
+++ app-user/src/main/java/com/example/appuser/controller/IDController.java
@@ -0,0 +1,24 @@
+package com.example.appuser.controller;
+
+import com.example.apputil.redis.api.IRedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/id")
+public class IDController {
+
+    @Autowired
+    IRedisService redisService;
+
+    @GetMapping("/getID/{msg}")
+    public String sendOnewayorder(@PathVariable String msg) {
+        return redisService.getID(msg);
+    }
+
+}
