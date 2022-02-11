84ae5ce370bcb739b3c59e4f1631da441aca303b
wangwc <wwc953@163.com>
2022-02-11 20:25:38 +0800

    还原

--- app-user/src/main/java/com/example/appuser/AppuserApplication.java
+++ app-user/src/main/java/com/example/appuser/AppuserApplication.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSwagger2
-@MapperScan("com.example.appuser.dao")
+@MapperScan(basePackages={"com.example.appuser.dao","com.example.apputil.redis.dao"})
 @SpringBootApplication(scanBasePackages = {"com.example.appuser", "com.example.apputil.*"})
 public class AppuserApplication {
 
