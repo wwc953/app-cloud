9201a3fa004a536d61291bdc32910662c6100e83
wangwc <wwc953@163.com>
2022-02-17 16:47:21 +0800

    延迟加载数据2

--- app-common/src/main/java/com/example/appcommon/model/SnoSt.java
+++ app-common/src/main/java/com/example/appcommon/model/SnoSt.java
@@ -1,5 +1,7 @@
 package com.example.appcommon.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class SnoSt {
@@ -15,6 +17,7 @@ public class SnoSt {
 
     private Long noLength;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date curDate;
 
     private Long initValue;
