b331b65e5a1d147e73a72829a0bbd2ab6f76a51d
wangwc <wwc953@163.com>
2022-02-17 15:01:10 +0800

    josnerror2

--- app-util/src/main/java/com/example/apputil/redis/remote/SignerFeignFallBack.java
+++ app-util/src/main/java/com/example/apputil/redis/remote/SignerFeignFallBack.java
@@ -3,7 +3,7 @@ package com.example.apputil.redis.remote;
 import java.util.Map;
 
 public class SignerFeignFallBack implements SignerFeign {
-    public static String ERROR_RES = "{\"msg\":\"远程调用异常\"}";
+    public static String ERROR_RES = null;
 
     @Override
     public String batchGenerateId(Map map) {
