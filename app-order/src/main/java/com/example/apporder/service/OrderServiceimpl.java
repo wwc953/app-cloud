1e7dc7f46aadb5a5c156a649893983197ecdaabf
wangwc <wwc953@163.com>
2022-02-19 19:19:44 +0800

    添加pom

--- app-order/src/main/java/com/example/apporder/service/OrderServiceimpl.java
+++ app-order/src/main/java/com/example/apporder/service/OrderServiceimpl.java
@@ -2,6 +2,7 @@ package com.example.apporder.service;
 
 import com.example.apporder.dao.OrderMapper;
 import com.example.apporder.model.Order;
+import com.example.appstaticutil.json.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,18 +16,19 @@ public class OrderServiceimpl {
     @Autowired
     OrderMapper orderMapper;
 
-    public Order getOrderId(Integer id){
+    public Order getOrderId(Integer id) {
         Order order = orderMapper.selectByPrimaryKey(id);
+        log.info("getOrderId result:{}", JsonUtil.convertObjectToJson(order));
         return order != null ? order : new Order();
     }
 
-    public Integer insertOrder(Order order){
-        if (order == null){
+    public Integer insertOrder(Order order) {
+        if (order == null) {
             log.error("order 不可为空！！");
         }
 
         int result = orderMapper.insert(order);
-        if (result < 0){
+        if (result < 0) {
             log.info("添加order失败！！！");
         }
         return result;
