01e72858d7cdf87e4fa506cb2c8411f5ecec45ff
wangwc <wwc953@163.com>
2022-02-23 14:11:46 +0800

    封装

--- app-static-util/src/main/java/com/example/appstaticutil/threadpool/ThreadPoolManager.java
+++ app-static-util/src/main/java/com/example/appstaticutil/threadpool/ThreadPoolManager.java
@@ -1,7 +1,7 @@
 package com.example.appstaticutil.threadpool;
 
-
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPoolManager {
 
@@ -10,7 +10,9 @@ public class ThreadPoolManager {
     private final ExecutorService executor;
 
     private ThreadPoolManager() {
-        executor = new ThreadPoolExecutor(0, 19999, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
+        executor = new ThreadPoolExecutor(0, 19999, 60L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                ThreadPoolManager.defaultThreadFactory());
     }
 
     public static ThreadPoolManager getInstance() {
@@ -32,5 +34,36 @@ public class ThreadPoolManager {
         return executor.submit(task);
     }
 
+
+    static class AppThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        AppThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = "app-" +
+                    poolNumber.getAndIncrement() +
+                    "-th-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
+    }
+
+    public static ThreadFactory defaultThreadFactory() {
+        return new ThreadPoolManager.AppThreadFactory();
+    }
 
 }
