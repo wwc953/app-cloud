ca36aa556684e3b7ebc97bd23eb6addb7b2bd5d3
wangwc <wwc953@163.com>
2022-04-20 15:36:36 +0800

    fallback response

--- app-user/src/main/java/com/example/appuser/remote/impl/OrderServiceFeignImpl.java
+++ app-user/src/main/java/com/example/appuser/remote/impl/OrderServiceFeignImpl.java
@@ -1,7 +1,16 @@
 package com.example.appuser.remote.impl;
 
 import com.example.appuser.remote.IOrderServiceFeign;
+import feign.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class OrderServiceFeignImpl implements IOrderServiceFeign {
     @Override
@@ -21,6 +30,12 @@ public class OrderServiceFeignImpl implements IOrderServiceFeign {
 
     @Override
     public feign.Response download(String fileName) {
-        return null;
+        return Response.builder()
+                .status(HttpStatus.OK.value())
+                .headers(new HashMap<String, Collection<String>>() {{
+                    put(HttpHeaders.CONTENT_TYPE, Arrays.asList(MediaType.APPLICATION_JSON_VALUE));
+                }})
+                .body("远程调用失败", Charset.forName("UTF-8"))
+                .build();
     }
 }
